72321c074f22f93c3c671da5979abb00e015761a
Nicholas Jones <me@nicholasjon.es>
2015-05-28 19:07:14 +0100

    Permit multiple input characters

--- lib/snowman/cli.rb
+++ lib/snowman/cli.rb
@@ -2,11 +2,15 @@ module Snowman
   class CLI
     def self.start
       value = ARGV[0]
+      length = value.length
+      br = $/ + ('─' * 58) + $/ + $/
 
-      raise 'A value must be supplied' unless value
-      raise 'A single character must be supplied' unless value.length == 1
+      raise 'A value must be supplied' unless length > 0
 
-      puts Character.new(value)
+      value.each_char.with_index(1) do |char, i|
+        puts Character.new(char)
+        puts br unless i == length
+      end
     end
   end
 end
